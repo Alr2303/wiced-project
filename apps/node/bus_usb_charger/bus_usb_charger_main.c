36e1219f97e8c831fbd1b21bd974f3dcd1a67a59
YongSung Lee <yslee@humminglab.io>
2018-05-18 12:13:09 +0900

    bumpbed version number usb_charger 0.0.6

--- apps/node/bus_usb_charger/bus_usb_charger_main.c
+++ apps/node/bus_usb_charger/bus_usb_charger_main.c
@@ -39,7 +39,7 @@
 #define QUOTE(str) #str
 #define EXPAND_AND_QUOTE(str) QUOTE(str)
 
-#define VERSION 0.0.5
+#define VERSION 0.0.6
 
 const char* fw_version = EXPAND_AND_QUOTE(VERSION);
 const char* fw_model = "USB-CHARGER";
